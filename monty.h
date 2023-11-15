2a2c0160996dd2328b9e65f5efbbc4c8233445bf
Manuel7070 <rubangakene3030ec@gmail.com>
2023-11-15 09:22:30 -0800

    added monty.h

---
+++ monty.h
@@ -0,0 +1,71 @@
+#ifndef MONTY_H
+#define MONTY_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <ctype.h>
+
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
+typedef struct stack_s
+{
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
+} stack_t;
+
+/**
+ * struct bus_s - variables -args, file, line content
+ * @arg: value
+ * @file: pointer to monty file
+ * @content: line content
+ * @lifi: flag change stack <-> queue
+ * Description: carries values through the program
+ */
+typedef struct bus_s
+{
+	char *arg;
+	FILE *file;
+	char *content;
+	int lifi;
+} bus_t;
+extern bus_t bus;
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
+typedef struct instruction_s
+{
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+int execute(char *content, stack_t **head, unsigned int counter, FILE *file);
+void free_stack(stack_t *head);
+void addnode(stack_t **head, int n);
+void addqueue(stack_t **head, int n);
+size_t get_strlen(const char *str);
+ssize_t _getline(char **line, size_t *len, FILE *stream);
+void queue_stack(stack_t **head, unsigned int counter);
+void stack_push(stack_t **head, unsigned int number);
+void stack_pall(stack_t **head, unsigned int number);
+void stack_pint(stack_t **head, unsigned int number);
+void stack_pop(stack_t **head, unsigned int counter);
+void stack_swap(stack_t **head, unsigned int counter);
+void add_to_stack(stack_t **head, unsigned int counter);
+void stack_nop(stack_t **head, unsigned int counter);
+#endif
