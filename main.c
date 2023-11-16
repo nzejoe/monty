8684aaa36edb4e78a3c6a0304082bb5038b4bc8f
nzejoe <jonathannzete@gmail.com>
2023-11-16 08:54:27 -0800

    main function pending

---
+++ main.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+bus_t bus = {NULL, NULL, NULL, 0};
+
+/**
+ * main - main code interpreter
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+	return (0);
+}
