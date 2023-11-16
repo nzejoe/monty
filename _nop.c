d8f433a3d0fc27232ecc1e9dd34b9a89851125a3
Manuel7070 <rubangakene3030ec@gmail.com>
2023-11-15 23:26:16 -0800

    Stack pop and pint functions

---
+++ _nop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+/**
+ *stack_nop- nothing
+ *@head: stack head
+ *@counter: line_number
+ *Return: no return
+ */
+void stack_nop(stack_t **head, unsigned int counter)
+{
+	(void)counter;
+	(void)head;
+}
